fa65f53f99d86cd6132e2077bf078fd9aca0be80
WP00N <87835024+WP00N@users.noreply.github.com>
2022-10-17 18:22:58 +1300

    gunicorn

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from redis import Redis
+import logging
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
@@ -10,5 +11,9 @@ def hello():
     counter = str(redis.get('hits'),'utf-8')
     return "Welcome to Thetips4you!, This webpage has been viewed "+counter+" time(s)"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", debug=True)
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
